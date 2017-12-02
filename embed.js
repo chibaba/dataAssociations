ffee72f4774f9ad621bf83d9b6606459e7a87cbf
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-12-02 19:29:26 +0100

    nesting callback hells

--- embed.js
+++ embed.js
@@ -1,30 +1,65 @@
 var mongoose = require ('mongoose');
 mongoose.connect("mongodb://localhost/blog_demo", { useMongoClient: true});
 
+var postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
 //User email-name
 var userSchema = new mongoose.Schema({
   email: String,
-  name: String
+  name: String,
+  posts: [postSchema]
 });
 
 var User = mongoose.model("User", userSchema);
 
 //POST - title, comment
-var postSchema = new mongoose.Schema({
-  title: String,
-  content: String
-});
 
 var post = mongoose.model("Post", postSchema);
 
-var newUser = new User({
-  email: "chibaba@brown.org",
-  name: "chibaba Brown"
-});
-newUser.save(function(err, user){
+// var newUser = new User({
+//   email: "ogwanu@brown.edu",
+//   name: "chinedu brown"
+// });
+
+// newUser.posts.push({
+//   title: "How to bre Polyjuice portion",
+//   content: "just kidding nothing of such"
+// });
+// newUser.save(function(err, user){
+//   if(err){
+//     console.log(err);
+//   }else {
+//     console.log(user);
+//   }
+// });
+
+// var newPost = new post({
+//   title: "Reflections on apples",
+//   content: "they are delicious"
+// }); 
+// newPost.save(function(err, Post){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(Post);
+//   }
+// })
+User.findOne({ name: "chinedu brown" }, function(err, user){
   if(err){
     console.log(err);
-  }else {
-    console.log(user);
+  } else {
+    user.posts.push({
+      title: "three things i dont like",
+      content: "never let your feRar decoide for you"
+    });
+    user.save(function(err, user){
+      if(err){
+        console.log(err);
+      }else {
+        console.log(user);
+      }
+    })
   }
-});
+})
